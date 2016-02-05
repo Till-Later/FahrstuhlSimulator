ee1b927223d48edffb319f7d3edce5ac22accce8
Till-Later <Lehmann@Mac.fritz.box>
2016-02-05 12:29:31 +0100

    Verzeichnisse und Klassen erstellt

---
+++ FahrstuhlSimulator/src/fahrstuhlsimulator/FahrstuhlSimulator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fahrstuhlsimulator;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+/**
+ *
+ * @author Lehmann
+ */
+public class FahrstuhlSimulator {
+    
+    public static int playstate = 1;
+    public static float speed = 1;
+    public static Calendar uhrzeit = new GregorianCalendar(2000,1,1,8,0,0);
+    public static Gebaeude gebaeude;
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
