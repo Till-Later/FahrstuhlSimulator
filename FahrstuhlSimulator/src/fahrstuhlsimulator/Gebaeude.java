6f725165c24f5c134b30a73090b269d053999f36
Till-Later <Lehmann@Mac.local>
2016-03-02 10:50:34 +0100

    Gebäude Methoden und Klassen erstellt

--- FahrstuhlSimulator/src/fahrstuhlsimulator/Gebaeude.java
+++ FahrstuhlSimulator/src/fahrstuhlsimulator/Gebaeude.java
@@ -5,10 +5,56 @@
  */
 package fahrstuhlsimulator;
 
+import java.util.ArrayList;
+
+
+
 /**
  *
  * @author Lehmann
  */
 public class Gebaeude implements tick {
+    private ArrayList<Etage> etagen;
+    private FahrstuhlController fahrstuhlcontroller;
+    private float etagenhoehe;
+    private int etagenZahl;
     
+    public Gebaeude () {
+       this.etagen = new ArrayList<Etage>();
+       this.fahrstuhlcontroller = new FahrstuhlController();
+       this.etagenhoehe = 3F;
+       this.etagenZahl = 8;
+    }
+    
+    public void setEtagenhoehe (float etagenhoehe) {
+        this.etagenhoehe = etagenhoehe;
+    }
+    public float getEtagenhoehe () {
+        return this.etagenhoehe;
+    }
+    
+    public void setEtagenzahl (int etagenZahl)  {
+        this.etagenZahl = etagenZahl;
+        if (this.etagenZahl < this.etagen.size()) {
+            while (this.etagenZahl < this.etagen.size()) {
+                this.etagen.remove(this.etagen.size()-1);
+            }
+        } else if (this.etagenZahl > this.etagen.size()) {
+            while (this.etagenZahl > this.etagen.size()) {
+                this.etagen.add(new Etage(this.etagen.size()+1));
+            }
+        }
+    }
+    
+    public int getEtagenzahl () {
+        return this.etagenZahl;
+    }
+    
+    public void starteNotraeumung () {
+    
+    }
+    
+    public void tick () {
+    
+    }
 }
