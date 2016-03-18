03f9ad884a1e83439c17ce3b9664a1d1bf3879d8
kevin <Klein@Kevin-Klein.local>
2016-03-18 10:51:25 +0100

    Fahrstuhlsimulator static methoden

--- FahrstuhlSimulator/src/fahrstuhlsimulator/FahrstuhlSimulator.java
+++ FahrstuhlSimulator/src/fahrstuhlsimulator/FahrstuhlSimulator.java
@@ -12,45 +12,43 @@ import java.util.GregorianCalendar;
  */
 public class FahrstuhlSimulator {
     
-    public static int playstate = 1;
-    public static float speed = 10;
+    public static int playstate = 0;
+    public static float speed = 1;
     public static Calendar uhrzeit = new GregorianCalendar(2000,1,1,8,0,0);
     public static Gebaeude gebaeude;
     public static GrafischesInterface grafischesInterface;
     
-    /**
-     * @param args the command line arguments
-     */
-    public void setPlaystate(int playstate){
-        this.playstate = playstate;
+    public static void setPlaystate(int ps){
+        playstate = ps;
     }
     
-    public int getPlaystate(){
+    public static int getPlaystate(){
         return playstate;
     }
     
-    public void setSpeed(float speed){
-        this.speed = speed;
+    public static void setSpeed(float s){
+        speed = s;
     }
     
-    public float getSpeed(){
+    public static float getSpeed(){
         return speed;
     }
     
-    public void setUhrzeit(Calendar uhrzeit){
-        this.uhrzeit = uhrzeit;
+    public static void setUhrzeit(Calendar uz){
+        uhrzeit = uz;
     }
     
-    public Calendar getUhrzeit(){
+    public static Calendar getUhrzeit(){
         return uhrzeit;
     }
-    
-    public static void tick(){
+    public static void tick()
+    {
         gebaeude.tick();
         grafischesInterface.tick();
     }
-  
-    public static void main(String[] args) {
+    
+    public static void main(String[] args)
+    {
         gebaeude = new Gebaeude();
         grafischesInterface = new GrafischesInterface();
         int i = 0;
@@ -65,5 +63,4 @@ public class FahrstuhlSimulator {
             }
         }
     }
-    
 }
